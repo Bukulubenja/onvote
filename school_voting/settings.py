e7b2530f41278dd0813276f2aaf4da162540d2c2
benvision <benjabukulu@gmail.com>
2026-06-17 10:37:17 +0300

    my second commit

--- school_voting/settings.py
+++ school_voting/settings.py
@@ -83,8 +83,7 @@ WSGI_APPLICATION = 'school_voting.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600
+        default=os.environ.get('DATABASE_URL')
     )
 }
 
