5ce5f1fcfed82064c01f20a100c2fe8d97733ca3
benvision <benjabukulu@gmail.com>
2026-06-17 11:04:03 +0300

    my forth commit

--- school_voting/settings.py
+++ school_voting/settings.py
@@ -82,9 +82,10 @@ WSGI_APPLICATION = 'school_voting.wsgi.application'
 
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get("DATABASE_URL")
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
+    }
 }
 
 AUTH_PASSWORD_VALIDATORS = [
